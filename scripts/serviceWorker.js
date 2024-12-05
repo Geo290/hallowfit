7f33ab20731b30709964eb783ffc178414f87008
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-12-05 14:01:39 -0600

    debuging: solving cache storage, still

--- scripts/serviceWorker.js
+++ scripts/serviceWorker.js
@@ -1,25 +1,25 @@
 // Name of the cache
-const CACHE_NAME = "portfolio_cache_v1";
+const CACHE_NAME = "portfolio_cache_v2";
 
 // Files to cache
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./styles/styles.css",
-  "./scripts/main.js",
-  "./imgs/icons/android-launchericon-48-48.png",
-  "./imgs/icons/android-launchericon-72-72.png",
-  "./imgs/icons/android-launchericon-96-96.png",
-  "./imgs/icons/android-launchericon-144-144.png",
-  "./imgs/icons/android-launchericon-192-192.png",
-  "./imgs/icons/android-launchericon-512-512.png",
-  "./imgs/backgrounds/city.jpg",
-  "./imgs/logo/emmanuel_portfolio_logo.png",
-  "./imgs/college/utna.jpg",
-  "./imgs/skills/skills.avif",
-  "./imgs/proyects/itosMansion.png",
-  "./imgs/proyects/sopiletras.png",
-  "./imgs/proyects/yournal-thumbnail.png",
+  "../",
+  "../index.html",
+  "../styles/styles.css",
+  "./main.js",
+  "../imgs/icons/android-launchericon-48-48.png",
+  "../imgs/icons/android-launchericon-72-72.png",
+  "../imgs/icons/android-launchericon-96-96.png",
+  "../imgs/icons/android-launchericon-144-144.png",
+  "../imgs/icons/android-launchericon-192-192.png",
+  "../imgs/icons/android-launchericon-512-512.png",
+  "../imgs/backgrounds/city.jpg",
+  "../imgs/logo/emmanuel_portfolio_logo.png",
+  "../imgs/college/utna.jpg",
+  "../imgs/skills/skills.avif",
+  "../imgs/proyects/itosMansion.png",
+  "../imgs/proyects/sopiletras.png",
+  "../imgs/proyects/yournal-thumbnail.png",
 ];
 
 self.addEventListener("install", (e) => {
